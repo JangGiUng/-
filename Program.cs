82a35245fc4fdd0c24ef6e4c35d8ed399e4ceeeb
JangGiUng <128966509+JangGiUng@users.noreply.github.com>
2023-03-27 11:13:24 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using System;
 using System.Collections;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 
 class Node
 {
@@ -103,81 +105,138 @@ class LinkedList
 }//링크드
 
 
+public class Array
+{
+    string[] arr;
+    int top;
+
+    public Array(int data)
+    {
+        this.arr = new string[data];
+        this.top = -1;
+
+    
+    }
+
+    public void push(string data)
+    {
+        this.arr[++top] = data;
+    }
+
+    public string pop()
+    {
+        String Data = "";
+        if (top==-1)
+        {
+            return "no";
+        }
+        else
+        {
+            Data = this.arr[this.top];
+            this.arr[this.top] = "";
+            this.top--;
+            return Data;
+        }
+        
+    }
+
+    public int leng()
+    {
+        return this.arr.Length;
+    }
+
+    public String arrHead()
+    {
+        
+        return arr[top];
+
+    }
+    public String arrTail()
+    {
+        int temp = 0;
+        String tail = arr[temp];
+        while (arr[++temp] != "")
+        {
+            tail = arr[++temp];
+        }
+        return tail;
+    }
+
+    public void retrun()
+    {
+        Console.Write("현재 배열 주차장에 주차된 차는 ");
+        if (arr[0] != "")
+        {
+
+            for (int i=0;i<this.arr.Length;i++)
+            {
+                Console.Write(arr[i] + "번");
+                int next = i + 1;
+                if (next!=arr.Length)
+                {
+                    Console.Write(",");
+
+                }
+
+            }
+            Console.WriteLine("입니다.");
+        }
+        else
+        {
+            Console.WriteLine("없습니다. ");
+        }
+
+
+    }
+
+
+}//배열
+
+
 class Report
 {
     static void Main(String[] args)
     {
         LinkedList stakckList= new LinkedList();
 
-        String[] stackArgs= new String[10];
+        Array stackArgs= new Array(10);
+
+        int leng=stackArgs.leng();
 
         Random parking=new Random();
 
-        for(int i=0; i<stackArgs.Length; i++)
+        for (int i = 0; i < leng; i++)
         {
             
             stakckList.push(parking.Next(1, 20) + "");
+            stackArgs.push(parking.Next(1, 20) + "");
 
-            stackArgs[i]= parking.Next(1, 20) + "";
-
-            Console.WriteLine("링크드 주차장에 {0}번 자동차가, 배열 주차장에는 {1}번 자동차가 주차되었습니다.", stakckList.LinkHead(), stackArgs[i]);
+            Console.WriteLine("링크드 주차장에 {0}번 자동차가, 배열 주차장에는 {1}번 자동차가 주차되었습니다.", stakckList.LinkHead(), stackArgs.arrHead());
             Console.WriteLine();
 
         }
 
         stakckList.retrun();
-
-        Console.Write("현재 배열 주차장에 주차된 차는 ");
-        if (stackArgs[0] != null)
-        {
-            for (int i = 0; i < stackArgs.Length; i++)
-            {
-                Console.Write(stackArgs[i]+"번");
-                if (i < stackArgs.Length - 1)
-                {
-                    Console.Write(", ");
-                }
-            }
-            Console.WriteLine("입니다. ");
-        }
-        else
-        {
-            Console.WriteLine("없습니다. ");
-        }
+        stackArgs.retrun();
         Console.WriteLine();
 
-
-        for (int i = stackArgs.Length-1; i >=0; i--)
+        for (int i = leng-1; i >= 0; i--)
         {
-            String key = stakckList.LinkTail();
-
-            Console.WriteLine("링크드 주차장에 {0}번 자동차가, 배열 주차장에는 {1}번 자동차가 빠져나갑니다.", key, stackArgs[i]);
-            Console.WriteLine();
+            String key=stakckList.LinkTail();
 
             stakckList.pop(key);
-            stackArgs[i] = "";
+
+            Console.WriteLine("링크드 주차장에 {0}번 자동차가, 배열 주차장에는 {1}번 자동차가 나갑니다.", key, stackArgs.pop());
+            Console.WriteLine();
 
         }
 
+
+        
         stakckList.retrun();
+        stackArgs.retrun();
 
-        Console.Write("현재 배열 주차장에 주차된 차는 ");
-        if (stackArgs[0] != "")
-        {
-            for (int i = 0; i < stackArgs.Length; i++)
-            {
-                Console.Write(stackArgs[i] + "번");
-                if (i < stackArgs.Length - 1)
-                {
-                    Console.Write(", ");
-                }
-            }
-            Console.WriteLine("입니다. ");
-        }
-        else
-        {
-            Console.WriteLine("없습니다. ");
-        }
+
 
 
     }
